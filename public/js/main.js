450a2628292de0fa872e6ee48391179def982021
TheodoraE <tidoraa@gmail.com>
2020-12-18 14:57:02 +0100

    style

--- public/js/main.js
+++ public/js/main.js
@@ -31,6 +31,7 @@ let div1Label = document.createElement('label');
 div1Label.innerHTML = "Ajouter une tâche :";
 
 let div1Input = document.createElement('input');
+div1Input.value = "";
 
 let boutonAjout = document.createElement('button');
 boutonAjout.innerHTML = "Ajouter";
@@ -58,9 +59,7 @@ for (let i = 0; i < 3; i++) {
 };
 
     //Div 3 Contenu
-let plan = [];
-divsC[2].append(plan)
-
+// Rien
 //////////////////////////////////////////////////////////
 ////////////////////// Style Général ////////////////////
 // body
@@ -108,111 +107,178 @@ for (let i = 0; i < allBoutons.length; i++) {
 
 //////////////////////////////////////////////////////////
 ////////////////////// Mise en fonction ////////////////////
-// Variables en dehors de l'Event
-let divTache;
-
-let divIcon;
-let iconCheck;
-let iconEdit;
-let iconDelete;
-let iconSave;
-let icons;
-
-let divSpan;
-let spanTache;
-
-let inputEdit;
-let inputEditValue;
-
 
 // Bouton Ajout
 boutonAjout.addEventListener('click', function(){
-    // div tâche (1 unité de tâche)
-    divTache = document.createElement('div');
-    divTache.setAttribute('class', "divTache");
-    divTache.style.backgroundColor = "Gainsboro";
-    divTache.style.height = "45px";
-    divTache.style.padding = "2%";
-    divTache.style.margin = "1% 0%";
-    divTache.style.borderRadius = "4px";
-    divsC[2].append(divTache);
+    if (div1Input.value != ""){
 
-        // div Span et span
-    divSpan = document.createElement('div');
-    spanTache = document.createElement('span');
-    spanTache.innerHTML = div1Input.value;
-    divSpan.append(spanTache);
+        // div tâche (1 unité de tâche)
+        let divTache = document.createElement('div');
+        divTache.setAttribute('class', "divTache");
+        divTache.style.backgroundColor = "Gainsboro";
+        divTache.style.height = "45px";
+        divTache.style.padding = "2%";
+        divTache.style.margin = "1% 0%";
+        divTache.style.borderRadius = "4px";
+        divsC[2].append(divTache);
 
-        // div Icon et icons
-    divIcon = document.createElement('div');
-            // Icon check
-    iconCheck = document.createElement('i');
-    iconCheck.setAttribute('class', "fas fa-check-circle");
-    iconCheck.style.marginRight = "25px";
-    iconCheck.style.color = "MediumTurquoise"
-            // Icon Edit
-    iconEdit = document.createElement('i');
-    iconEdit.setAttribute('class', "fas fa-edit");
-    iconEdit.style.marginRight = "25px";
-    iconEdit.style.color = "Khaki"
-            // Icon Delete
-    iconDelete = document.createElement('i');
-    iconDelete.setAttribute('class', "fas fa-trash-alt");
-    iconDelete.style.marginRight = "25px";
-    iconDelete.style.color = "red";
-    iconDelete.setAttribute('id', "iconDel");
-    divIcon.append(iconCheck, iconEdit, iconDelete);
+            // div Span et span
+        let divSpan = document.createElement('div');
+        let spanTache = document.createElement('span');
+        spanTache.innerHTML = div1Input.value;
+        divSpan.append(spanTache);
 
-    divTache.style.display = "flex";
-    divTache.style.justifyContent = "space-between";
-    // Tous les icons
-    icons = divIcon.querySelectorAll('i');
+            // div Icon et icons
+        let divIcon = document.createElement('div');
+                // Icon check
+        let iconCheck = document.createElement('i');
+        iconCheck.setAttribute('class', "fas fa-check-circle");
+        iconCheck.style.marginRight = "25px";
+        iconCheck.style.color = "MediumTurquoise"
+                // Icon Edit
+        let iconEdit = document.createElement('i');
+        iconEdit.setAttribute('class', "fas fa-edit");
+        iconEdit.style.marginRight = "25px";
+        iconEdit.style.color = "Khaki"
+                // Icon Delete
+        let iconDelete = document.createElement('i');
+        iconDelete.setAttribute('class', "fas fa-trash-alt");
+        iconDelete.style.marginRight = "25px";
+        iconDelete.style.color = "red";
+        iconDelete.setAttribute('id', "iconDel");
+        divIcon.append(iconCheck, iconEdit, iconDelete);
 
-    divTache.append(divSpan, divIcon)
+        divTache.style.display = "flex";
+        divTache.style.justifyContent = "space-between";
+        // Tous les icons
+        let icons = divIcon.querySelectorAll('i');
 
-    // Valeur de Input
-    div1Input.value = "";
-    // console.log(icons) 
-    
-    // 1er icon : Valider la tâche
-    iconCheck.addEventListener('click', function(){
-        if(divTache.style.backgroundColor == "green"){
-            divTache.style.backgroundColor = "Gainsboro";
-        } else {
-            divTache.style.backgroundColor = "green";
-        }
-    });
-    // 2ème icon : Modifier la tâche
-    iconEdit.addEventListener('click', function(){
-        spanTache.style.display = "none";
-        iconCheck.style.display = "none";
-        iconEdit.style.display = "none";
-        iconDelete.style.display = "none";
-        // icone Sauvegarder
-        iconSave = document.createElement('i');
-        iconSave.setAttribute('class', "fas fa-save");
-        iconSave.style.marginRight = "20px";
-        divIcon.append(iconSave);
+        divTache.append(divSpan, divIcon)
 
-        // Sauvegarder
-        iconSave.addEventListener('click', function(){
-            inputEditValue = inputEdit.value;
-            inputEdit.style.display = "none";
-            spanTache.innerHTML = inputEditValue;
-            spanTache.style.display = "block";
-            iconCheck.style.display = "inline-block";
-            iconEdit.style.display = "inline-block";
-            iconDelete.style.display = "inline-block";
-            iconSave.style.display = "none";
+        // Valeur de Input
+        div1Input.value = "";
+        
+        // 1er icon : Valider la tâche
+        iconCheck.addEventListener('click', function(){
+            if(divTache.style.backgroundColor == "green"){
+                divTache.style.backgroundColor = "Gainsboro";
+            } else {
+                divTache.style.backgroundColor = "green";
+            }
         });
+        // 2ème icon : Modifier la tâche
+        iconEdit.addEventListener('click', function(){
+            spanTache.style.display = "none";
+            iconCheck.style.display = "none";
+            iconEdit.style.display = "none";
+            iconDelete.style.display = "none";
+            // icone Sauvegarder
+            let iconSave = document.createElement('i');
+            iconSave.setAttribute('class', "fas fa-save");
+            iconSave.style.marginRight = "20px";
+            iconSave.style.color = "Khaki"
+            divIcon.append(iconSave);
 
-        // Input pour modifier
-        inputEdit = document.createElement('input');
-        divSpan.append(inputEdit);
-    });
+            // Sauvegarder
+            iconSave.addEventListener('click', function(){
+                inputEditValue = inputEdit.value;
+                inputEdit.style.display = "none";
+                spanTache.innerHTML = inputEditValue;
+                spanTache.style.display = "block";
+                iconCheck.style.display = "inline-block";
+                iconEdit.style.display = "inline-block";
+                iconDelete.style.display = "inline-block";
+                iconSave.style.display = "none";
+            });
 
-    //
+            // Input pour modifier
+            let inputEdit = document.createElement('input');
+            divSpan.append(inputEdit);
 
-})
+            // Sauvegarder avec la touche ENTER
+            body.addEventListener('keydown', function(e){
+                if (e.keyCode === 13) {
+                    if (inputEdit.value != ""){
+                        iconSave.click();
+                    };
+                };
+            });
+        });
+        // 3ème icon : Supprimer la tâche
+        iconDelete.addEventListener('click', function(){
+            // Display
+            spanTache.style.display = "none";
+            iconCheck.style.display = "none";
+            iconEdit.style.display = "none";
+            iconDelete.style.display = "none";
+            // Créer un dexième span pour le remplacer
+            let spanTache2 = document.createElement('span');
+            spanTache2.innerHTML = "Êtes-vous sûr de vouloir supprimer cette tâche ?";
+            divSpan.append(spanTache2);
+            // Créer des boutons OUI et NON
+            let boutonOUI = document.createElement('button');
+            boutonOUI.innerHTML = "OUI";
+            boutonOUI.style.backgroundColor = "green";
+            boutonOUI.style.color = "white";
+            boutonOUI.style.marginRight = "5px";
+            boutonOUI.style.borderRadius = "5px";
+            let boutonNON = document.createElement('button');
+            boutonNON.innerHTML = "NON";
+            boutonNON.style.backgroundColor = "red";
+            boutonNON.style.color = "white";
+            boutonNON.style.marginRight = "5px";
+            boutonNON.style.borderRadius = "5px";
+            divIcon.append(boutonOUI, boutonNON);
 
+            // Bouton OUI
+            boutonOUI.addEventListener('click', function(){
+                divsC[2].removeChild(divTache);
+            });
+            // Bouton NON
+            boutonNON.addEventListener('click', function(){
+                iconCheck.style.display = "inline-block";
+                iconEdit.style.display = "inline-block";
+                iconDelete.style.display = "inline-block";
+                boutonOUI.style.display = "none";
+                boutonNON.style.display = "none";
+                spanTache.style.display = "block";
+                spanTache2.style.display = "none";
+            });
+        });
+        
+        //// Boutons ACTIONS
+        let tacheAll = divsC[2].querySelectorAll('.divTache');
 
+        for (let i = 0; i < tacheAll.length; i++) {
+            // bouton "À faire"
+            boutonsC2[0].addEventListener('click', function(){
+                if (tacheAll[i].style.backgroundColor != "green") {
+                    tacheAll[i].style.display = "flex";
+                } else {
+                    tacheAll[i].style.display = "none";
+                };
+            });
+            // bouton "Terminé"
+            boutonsC2[1].addEventListener('click', function(){
+                if (tacheAll[i].style.backgroundColor != "green") {
+                    tacheAll[i].style.display = "none";
+                } else {
+                    tacheAll[i].style.display = "flex";
+                };
+            });
+            // bouton "Tous"
+            boutonsC2[2].addEventListener('click', function () {
+                tacheAll[i].style.display = 'flex'
+            });
+        };
+    };
+});
+
+// Ajouter une tâche avec la touche ENTER
+body.addEventListener('keydown', function(e){
+    if (e.keyCode=== 13){
+        if (div1Input.value != ""){
+            boutonAjout.click()
+        };
+    };
+});
